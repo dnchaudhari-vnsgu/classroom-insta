4d9681f5719372ea213c6dc3c0967aaa59af43e7
Divyangi <82317895+Divyangi17@users.noreply.github.com>
2025-09-01 18:24:14 +0530

    print username

--- view_post.php
+++ view_post.php
@@ -4,14 +4,18 @@ include 'nav.php';
 
 $current_user = $_SESSION['user_id'];
 
-$sql = "SELECT * from posts where id={$_GET['post_id']}";
+$sql = "SELECT posts.*, users.username, users.profile_pic 
+        FROM posts 
+        JOIN users ON posts.user_id = users.id 
+        WHERE posts.id = {$_GET['post_id']}";
+echo $sql;
 $result = $conn->query($sql);
 
 echo "<h2>Post</h2>";
 echo "<table border='1' width='100%'>";
 while ($row = $result->fetch_assoc()) {
     
-    echo "<tr><td> {$row['content']}</td></tr>";
+    echo "<tr><td><b>{$row['username']}</b> : {$row['content']}</td></tr>";
 }
 echo "</table>";
 echo "<br><br><textarea id='comment' placeholder='comment here'> </textarea> <br><button>Add comment</button>"; 
